a5741987355f6b9b4e2cd2700dd8de6263c223cd
osminPeña <osminv91@gamil.com>
2025-10-05 18:15:43 -0500

    Agregando efecto de scroll

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const ol = document.getElementById('header-ol')
 nav.addEventListener('click',() =>{
     ol.classList.toggle('active')
 })
+
+
+const scrollVision = () =>{
+    const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target); // Para que solo ocurra una vez
+      }
+    });
+  });
+  document.querySelectorAll('.hidden').forEach(el => observer.observe(el));
+
+}
+
+scrollVision()
